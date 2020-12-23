b11f22813603bbf3a6f8a4851dad1bb2248c40d1
Mohammad Rebati <m.rebati@icloud.com>
2020-12-23 12:00:05 +0330

    fix #2 added BridgeDestroy Action

---
+++ AsterNET.NetStandard/Manager/Action/BridgeDestroy.cs
@@ -0,0 +1,25 @@
+﻿namespace AsterNET.NetStandard.Manager.Action
+{
+    
+    public class BridgeDestroy : ManagerAction
+    {
+        /// <summary>
+        /// Deletes the bridge, causing channels to continue or hang up.
+        /// </summary>
+        /// <param name="bridgeUniqueId">Bridge UniqueId</param>
+        public BridgeDestroy(string bridgeUniqueId)
+        {
+            BridgeUniqueId = bridgeUniqueId;
+        }
+
+        public override string Action
+        {
+            get { return "Bridge"; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string BridgeUniqueId { get; set; }
+    }
+}
